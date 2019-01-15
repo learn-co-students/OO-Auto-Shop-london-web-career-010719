d068aa191da10687879f6f43ec5dd19a2fc98ae4
kkoomin <minha.koo.9@gmail.com>
2019-01-15 13:46:57 +0000

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,31 @@
 class Car
 
-  attr_reader :make, :model
+  attr_accessor :car_owner
+  attr_reader :make, :model, :classification, :mechanic
+  @@all = []
 
   def initialize(make, model, classification)
     @make = make
     @model = model
     @classification = classification
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def mechanic=(mechanic)
+    @mechanic = mechanic if mechanic.specialty == self.classification
+  end
+
+  def self.classifications
+    @@all.map {|car| car.classification}.uniq
+  end
+
+  def mechanics
+    Mechanic.all.select {|mechanic| mechanic.specialty == self.classification}
+  end
+
 
 end
